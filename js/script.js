e03eb1f47ed45bee5d43cc92a9a2a74ab72295b1
Bozelo <bozelojr@hotmail.com>
2022-04-30 12:31:07 -0300

    Aplica funcionalidade do menu hamburguer com JavaScript, aplicaresponsividade

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-// alert("JavaScript externo esta funcionando")
+
+// estrutura "funcao()"" pega uma serie de comandos e da um nome para conseguir chamar de um outro lugar(script).
+function mostrarMenu() {
+    let menu = document.getElementById("menu")
+
+    if (getComputedStyle(menu).display == 'none') {
+        menu.style.display = 'flex';
+    } else {
+menu.style.display = 'none';
+    }
+}
+
+function cadastrarNewsletter() {
+   
+    let nome = document.getElementById("input1").value
+    let senha = document.getElementById("input2").value
+
+    // alert(nome)
+    // alert(senha)
+
+    console.log(nome, senha)
+
+}
 
